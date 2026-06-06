046c13365e8df7313198a7b604e4707b50f090bd
CodeDiddy <nielsbakker1978@gmail.com>
2026-06-06 17:54:57 +0200

    javadoc toegevoegd en testklassen

--- src/domein/PrikToGo.java
+++ src/domein/PrikToGo.java
@@ -2,19 +2,34 @@ package domein;
 
 import data.Mapper;
 
+/**
+ * Kapstokklasse voor de Prik2Go applicatie.
+ * Beheert alle vestigingen en hun klanten.
+ */
 public class PrikToGo {
 
     private final Vestiging[] vestigingen;
 
+    /**
+     * Maakt een nieuwe PrikToGo aan en laadt alle vestigingen uit de database.
+     */
     public PrikToGo() {
-        
         vestigingen = Mapper.getVestigingen();
     }
 
+    /**
+     * Geeft de klantenlijst van een geselecteerde vestiging terug.
+     * @param id index van de vestiging
+     * @return array van klantnummers als strings
+     */
     public String[] selecteerVestiging(int id) {
         return vestigingen[id].getKlantenInfo();
     }
 
+    /**
+     * Geeft de plaatsnamen van alle vestigingen terug.
+     * @return array van plaatsnamen
+     */
     public String[] getOverzichtVestigingen() {
         String[] reStrings = new String[vestigingen.length];
         for (int i = 0; i < reStrings.length; i++) {
